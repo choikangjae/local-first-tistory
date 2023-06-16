c5868dbc45a2c98ba9749a23524402f302052ef8
choikangjae <choikj33@gmail.com>
2023-06-16 18:37:47 +0900

    feat: Change image id from the relative path to SHA1

--- image.py
+++ image.py
@@ -2,6 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 import configparser
+import hashlib
 
 load_dotenv()
 image_info = configparser.ConfigParser()
@@ -26,9 +27,10 @@ def upload_file(path):
     res = requests.post(upload_url, data=upload_file_params, files=files).json()
     return res['tistory']['url']
 
-def save_image_url(image_rel_path, url):
-    image_info[image_rel_path] = {}
-    image_info[image_rel_path]['url'] = url
+def save_image_url(image_hash: str, image_rel_path: str, url: str):
+    image_info[image_hash] = {}
+    image_info[image_hash]['path'] = image_rel_path
+    image_info[image_hash]['url'] = url
     image_info.write(open(IMAGE_INFO_PATH, 'w'))
     print(f"이미지가 티스토리 서버에 저장되었습니다. image = {image_rel_path} url = {url}")
 
@@ -38,13 +40,19 @@ def traverse_images():
         for file in files:
             if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
                 image_rel_path = os.path.join(subdir, file)
+                with open(image_rel_path, "rb") as f:
+                    image_hash = hashlib.sha1(f.read()).hexdigest()
 
                 image_info.read(IMAGE_INFO_PATH)
                 # If saved images does not exist, upload the post and save the metadata
-                if image_rel_path not in image_info:
+                if image_hash not in image_info:
                     url = upload_file(image_rel_path)
-                    save_image_url(image_rel_path, url)
+                    save_image_url(image_hash, image_rel_path, url)
                     count += 1
+                # If image is saved but the path is changed, modify the path of image.
+                elif image_hash in image_info and image_info[image_hash]['path'] != image_rel_path:
+                    image_info[image_hash]['path'] = image_rel_path
+                    image_info.write(open(IMAGE_INFO_PATH, 'w'))
 
     print(f"총 {count} 개의 이미지가 저장되었습니다. url은 '{IMAGE_INFO_PATH}'에서 확인하실 수 있습니다")
 
