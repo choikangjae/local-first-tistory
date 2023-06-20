db2a015788a770ec5fb0820cef7ad1539f56b88c
choikangjae <choikj33@gmail.com>
2023-06-20 15:58:15 +0900

    feat: Make env.py to hold all the const

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-program="tistory.py"
-pyinstaller --onefile $program --hidden-import mdx_truly_sane_lists --hidden-import markdown_link_attr_modifier
-
--- init.py
+++
@@ -1,6 +0,0 @@
-from auth import auth
-from category import load_categories_from_tistory
-
-if __name__ == "__main__":
-    auth()
-    load_categories_from_tistory()
